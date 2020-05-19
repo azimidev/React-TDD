7f9fa308fbea27520e78331bd9d0fc8bdb3aa9d9
Hassan Azimi <hassanazimi@users.noreply.github.com>
2020-05-19 18:26:48 +0100

    writing tests for app components

--- 3-Jotto-Redux/src/components/App/App.js
+++ 3-Jotto-Redux/src/components/App/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react'
 import Congrates from '../Congrats/Congrates'
 import GuessedWords from '../GuessedWords/GuessedWords'
+import { connect } from 'react-redux'
+import { getSecretWord } from '../../actions'
+import { Input } from '../Input/Input'
 
-export default class App extends Component {
+class App extends Component {
     render() {
         return (
             <div className="container">
                 <h1 className="title">Jotto</h1>
-                <Congrates success={true} />
-                <GuessedWords
-                    guessedWords={[
-                        { guessedWord: 'train', letterMatchCount: 3 },
-                    ]}
-                />
+                <Congrates success={this.props.success} />
+                <Input />
+                <GuessedWords guessedWords={this.props.guessedWords} />
             </div>
         )
     }
 }
+
+const mapStateToProps = (state) => ({
+    success: state.success,
+    guessedWords: state.guessedWords,
+    secretWord: state.secretWord,
+})
+
+const mapDispatchToProps = { getSecretWord }
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- 3-Jotto-Redux/src/components/App/App.test.js
+++ 3-Jotto-Redux/src/components/App/App.test.js
@@ -1,19 +1,38 @@
 import React from 'react'
 import App from './App'
 import { shallow } from 'enzyme'
+import { storeFactory } from '../../test/testUtils'
 
-// We don'e need this now but if you do please uncomment:
-// import { findByTestAttr } from "../../test/testUtils"
-
-const setup = (props = {}, state = null) => {
-    const wrapper = shallow(<App {...props} />)
-    if (state) wrapper.setState(state)
+const setup = (state = {}) => {
+    const store = storeFactory(state)
+    const wrapper = shallow(<App store={store} />)
+        .dive()
+        .dive()
     return wrapper
 }
 
-describe('App components', () => {
-    test('should render the App component', () => {
+describe('redux properties', () => {
+    test('should have access to `success` state', () => {
+        const success = true
+        const wrapper = setup({ success })
+        const successProp = wrapper.instance().props.success
+        expect(successProp).toBe(success)
+    })
+    test('should have access to `secretWord` state', () => {
+        const secretWord = 'pary'
+        const wrapper = setup({ secretWord })
+        const secretWordProp = wrapper.instance().props.secretWord
+        expect(secretWordProp).toBe(secretWord)
+    })
+    test('should have access to `guessWords` state', () => {
+        const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }]
+        const wrapper = setup({ guessedWords })
+        const guessedWordsProp = wrapper.instance().props.guessedWords
+        expect(guessedWordsProp).toBe(guessedWords)
+    })
+    test('should `getSecretWord` action creator as a function on the props', () => {
         const wrapper = setup()
-        expect(wrapper).toBeTruthy()
+        const getSecretWordProp = wrapper.instance().props.getSecretWord
+        expect(getSecretWordProp).toBeInstanceOf(Function)
     })
 })
